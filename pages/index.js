aee6eb67eb36581a57f94fb458f7851c35a31314
wittayasutt <wittayasutt@gmail.com>
2017-12-15 18:35:10 +0700

    tim

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Fork from '../components/Fork'
 import Todo from '../components/Todo'
 
 import initStore from '../utils/store'
+import moment from 'moment'
 
 class Index extends React.Component {
 	static async getInitialProps({ store }) {
@@ -23,10 +24,15 @@ class Index extends React.Component {
 		login: false,
 		accessToken: null,
 		name: '',
+		time: moment()
+			.subtract({ minutes: 10 })
+			.format(),
 		keyword: '',
 		selectedGroup: -1,
+		selectedPost: -1,
 		groupResult: [],
-		feed: []
+		feed: [],
+		comment: []
 	}
 
 	fbStatus = () => {
@@ -73,52 +79,84 @@ class Index extends React.Component {
 		let { keyword, groupResult } = this.state
 		let _this = this
 
-		if (groupResult.length === 0) {
-			FB.api('/search', { type: 'group', q: keyword }, response => {
-				if (response) _this.setState({ groupResult: response.data })
-			})
-		}
+		FB.api('/search', { type: 'group', q: keyword }, response => {
+			if (response)
+				_this.setState({
+					selectedGroup: -1,
+					groupResult: response.data
+				})
+		})
 	}
 
 	selectGroup = id => {
 		let _this = this
-		// FB.api(`/${id}/feed`, { accessToken: this.state.accessToken }, response => {
-		// 	if (response) {
-		// 		_this.setState(
-		// 			{
-		// 				feed: response.data,
-		// 				selectedGroup: id
-		// 			},
-		// 			() => {
-		// 				console.log('this.state.feed', this.state.feed)
+		FB.api(
+			`/${id}/feed?since=${moment()
+				.subtract({ minutes: 30 })
+				.format()}`,
+			{ accessToken: this.state.accessToken },
+			response => {
+				if (response) {
+					_this.setState(
+						{
+							feed: response.data,
+							selectedGroup: id
+						},
+						() => {
+							this.state.feed.forEach((post, index) => {
+								FB.api(`/${post.id}`, { fields: 'picture' }, response => {
+									if (response.picture) {
+										let feed = this.state.feed
+										feed[index].picture = response.picture
+										_this.setState({ feed })
+									}
+								})
+							})
+						}
+					)
+				}
+			}
+		)
+	}
 
-		// 				this.state.feed.forEach((post, index) => {
-		// 					FB.api(`/${post.id}`, { fields: 'picture' }, response => {
-		// 						if (response.picture) {
-		// 							let feed = this.state.feed
-		// 							feed[index].picture = response.picture
-		// 							_this.setState({ feed })
-		// 						}
-		// 					})
-		// 				})
-		// 			}
-		// 		)
-		// 	}
-		// })
+	closeGroup = () => {
+		this.setState({
+			selectedGroup: -1,
+			feed: []
+		})
+	}
+
+	seeComment = id => {
+		let _this = this
+		FB.api(`/${id}/comments`, {}, response => {
+			this.setState(
+				{
+					selectedPost: id,
+					comment: response.data
+				},
+				() => {
+					this.state.comment.forEach((comment, index) => {
+						FB.api(`/${comment.id}/comments`, {}, response => {
+							let comment = this.state.comment
+							comment[index].reply = response.data
+							_this.setState({ comment })
+						})
+					})
+				}
+			)
+		})
 	}
 
 	setGroup = () => {
-		let { groupResult } = this.state
+		let { selectedGroup, groupResult } = this.state
 		let items = []
 
-		console.log('groupResult', groupResult)
-
-		if (groupResult.length > 0) {
+		if (selectedGroup === -1 && groupResult.length > 0) {
 			let key = 0
 			groupResult.forEach(item => {
 				items.push(
-					<li onClick={this.selectGroup(item.id)} key={key}>
-						<a>
+					<li key={key}>
+						<a onClick={e => this.selectGroup(item.id)}>
 							[{item.privacy}] {item.name}
 						</a>
 
@@ -140,26 +178,109 @@ class Index extends React.Component {
 	}
 
 	setFeed = () => {
-		let { feed } = this.state
+		let { selectedPost, feed } = this.state
 		let items = []
 
 		if (feed.length > 0) {
 			let key = 0
 			feed.forEach(item => {
 				items.push(
-					<li key={key}>
-						{items.message}
+					<div className="demo-card-wide mdl-card mdl-shadow--2dp" key={key}>
+						<div className="new-post">NEW</div>
+						{item.picture ? (
+							<div className="mdl-card__title">
+								<img className="image" src={item.picture} alt={item.message} />
+							</div>
+						) : null}
+						<div className="mdl-card__supporting-text">{item.message}</div>
+						<div className="mdl-card__actions mdl-card--border">
+							{selectedPost === item.id ? (
+								this.setComment()
+							) : (
+								<a
+									className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+									onClick={e => this.seeComment(item.id)}>
+									Comment
+								</a>
+							)}
+						</div>
 
 						<style jsx>{`
-							a {
-								cursor: pointer;
+							.demo-card-wide {
+								min-height: auto;
+								width: 100%;
+								margin-top: 2rem;
 							}
-							a:hover {
-								text-decoration: underline;
+							.new-post {
+								background: #448aff;
+								color: #ffffff;
+								padding: 10px;
+							}
+							.image {
+								width: 100%;
 							}
 						`}</style>
-					</li>
+					</div>
 				)
+				key++
+			})
+		}
+
+		return items
+	}
+
+	setComment = () => {
+		let { time, comment } = this.state
+		let items = []
+
+		if (comment.length > 0) {
+			let key = 0
+			comment.forEach(item => {
+				const newRep =
+					moment(item.created_time).diff(time, 'seconds') > 0
+						? 'mdl-chip mdl-chip--contact mdl-badge mdl-badge--overlap'
+						: 'mdl-chip mdl-chip--contact'
+
+				items.push(
+					<span className={newRep} data-badge="N" key={key}>
+						<span className="mdl-chip__contact mdl-color--teal mdl-color-text--white">
+							C
+						</span>
+						<span className="mdl-chip__text">{item.message}</span>
+
+						<style jsx>{`
+							.mdl-chip--contact {
+								display: block;
+							}
+						`}</style>
+					</span>
+				)
+
+				if (item.reply) {
+					item.reply.forEach(rep => {
+						const newRep =
+							moment(rep.created_time).diff(time, 'seconds') > 0
+								? 'mdl-chip mdl-chip--contact mdl-badge mdl-badge--overlap'
+								: 'mdl-chip mdl-chip--contact'
+
+						key++
+						items.push(
+							<span className={newRep} data-badge="N" key={key}>
+								<span className="mdl-chip__contact mdl-color--red mdl-color-text--white">
+									R
+								</span>
+								<span className="mdl-chip__text">{rep.message}</span>
+
+								<style jsx>{`
+									.mdl-chip--contact {
+										display: block;
+										margin-left: 1.5rem;
+									}
+								`}</style>
+							</span>
+						)
+					})
+				}
 				key++
 			})
 		}
@@ -172,7 +293,7 @@ class Index extends React.Component {
 		let { login, name, keyword, selectedGroup } = this.state
 
 		let groups = this.setGroup()
-		// let feed = this.setFeed()
+		let feed = this.setFeed()
 
 		if (!login) {
 			return (
@@ -227,8 +348,10 @@ class Index extends React.Component {
 
 					{selectedGroup !== -1 ? (
 						<div>
-							x
-							<ul>{}</ul>
+							<span className="close" onClick={e => this.closeGroup()}>
+								x
+							</span>
+							{feed}
 						</div>
 					) : (
 						<ul>{groups}</ul>
@@ -259,6 +382,13 @@ class Index extends React.Component {
 						}
 						.mdl-card li:hover {
 							text-decoration: underline;
+						}
+						.mdl-card span.close {
+							float: right;
+							cursor: pointer;
+						}
+						.mdl-card span.close:hover {
+							opacity: 0.8;
 						}
 					`}</style>
 				</div>
